eaa64a831b105226c19a0d38f039b9678b0ebcd2
Asif Khan <akhan@codeinreach.com>
2019-06-30 21:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string 
+  if string === string.toLowerCase
+  return "I can't hear you!"
 }
 
 function sayHiToGrandma(string) {
