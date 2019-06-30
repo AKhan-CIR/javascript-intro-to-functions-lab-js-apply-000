d50385d322a41a1ce72951ecd509d2d99b799d09
Asif Khan <akhan@codeinreach.com>
2019-06-30 21:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase())
-  return "I can't hear you!"
+  if (string === string.toUpperCase())
+  return "YES INDEED!"
 }
 
 function sayHiToGrandma(string) {
-  if string === "I love you, Grandma."
+  if (string === "I love you, Grandma.")
   return "I love you, too."
 }
